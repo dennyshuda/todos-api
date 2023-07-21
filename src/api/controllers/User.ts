23c690dff36d4f2792f9be916b4084d34ccc3157
Denny Setyawan Huda <denycam9@gmail.com>
2023-07-22 06:43:12 +0800

    feat: login controller

--- src/api/controllers/User.ts
+++ src/api/controllers/User.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { hashing } from "../utils/hashing";
 import { IUser } from "../types";
 import userServices from "../services/User";
+import jwt from "jsonwebtoken";
 
 const register = async (req: Request, res: Response) => {
   const { name, email, password }: IUser = req.body;
@@ -32,6 +33,37 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
-const userController = { register };
+const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userServices.login(email, password);
+    const getUser = await userServices.getUserByEmail(email);
+
+    if (user) {
+      const accessToken = jwt.sign(
+        { id: getUser?.id, name: getUser?.name, email: getUser?.email },
+        process.env.ACCESS_TOKEN_SECRET!
+      );
+      return res.status(200).json({
+        status: "succes",
+        message: "password is match",
+        token: accessToken,
+      });
+    } else {
+      return res.status(400).json({
+        status: "wrong password",
+        message: "password is NOT match",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "something wrong",
+    });
+  }
+};
+
+const userController = { register, login };
 
 export default userController;
