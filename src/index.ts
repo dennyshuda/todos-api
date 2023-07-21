85f60474dd7b6433de6999a64be98687d160a276
Denny Setyawan Huda <denycam9@gmail.com>
2023-07-21 09:24:37 +0800

    feat: import dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Application } from "express";
 import HealthRouter from "./api/routes/Health";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
 
-const PORT: number = 5000;
+const PORT = process.env.PORT;
 
 app.use(HealthRouter);
 
