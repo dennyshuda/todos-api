c500bf907f2b6df7d500d51311d2120e8ca701b7
Denny Setyawan Huda <denycam9@gmail.com>
2023-07-21 13:45:18 +0800

    feat: import cors json and user router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express, { Application } from "express";
 import HealthRouter from "./api/routes/Health";
 import dotenv from "dotenv";
+import cors from "cors";
+import UserRouter from "./api/routes/User";
 
 dotenv.config();
 
 const app: Application = express();
-
 const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors());
 
 app.use(HealthRouter);
+app.use(UserRouter);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
