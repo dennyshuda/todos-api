4ce5c269f6a8f3149b48ef7cc83f8ffee6e7f99a
Denny Setyawan Huda <denycam9@gmail.com>
2023-07-22 06:44:12 +0800

    feat: add router login

--- src/api/routes/User.ts
+++ src/api/routes/User.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/User";
 const UserRouter = Router();
 
 UserRouter.post("/register", userController.register);
+UserRouter.post("/login", userController.login);
 
 export default UserRouter;
